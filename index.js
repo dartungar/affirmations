d516cb0fb6dc08541f674e77645f82c5405d500d
dartungar <dartungar@gmail.com>
2020-10-13 14:04:00 +0300

    fixed body margin; new initial affirmation

---
+++ index.js
@@ -0,0 +1,27 @@
+import changeCardAndIcon from "./helpers.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  //define main elements we'll be working with
+  let reloadBtn = document.querySelector(".renew-btn");
+  let container = document.querySelector(".main");
+  let card = document.querySelector(".card");
+  let icon = document.querySelector(".hero-icon");
+
+  // on clicking 'reload' button, change card & icon
+  reloadBtn.addEventListener("click", () => {
+    changeCardAndIcon(container, card, icon);
+  });
+
+  //register service worker
+  if ("serviceWorker" in navigator) {
+    console.log("registering service worker...");
+    navigator.serviceWorker
+      .register("worker.js")
+      .then(console.log("service worker registered!"));
+  }
+});
+
+// for A2HS for desktop
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.prompt();
+});
